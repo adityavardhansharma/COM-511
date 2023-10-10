26b560071af73f81e7ca7225dfd52134ec1a694d
Aditya Vardhan Sharma <adityaradhika13@gmail.com>
2023-10-10 13:17:22 +0530

    Create exp3.py

---
+++ exp3.py
@@ -0,0 +1,18 @@
+# Iterating over a list
+fruits = ["apple", "banana", "cherry", "date"]
+
+print("Iterating over a list:")
+for fruit in fruits:
+    print(fruit)
+
+# Iterating over a dictionary
+student_info = {
+    "name": "John",
+    "age": 20,
+    "major": "Computer Science",
+    "gpa": 3.5
+}
+
+print("\nIterating over a dictionary:")
+for key, value in student_info.items():
+    print(f"{key}: {value}")
